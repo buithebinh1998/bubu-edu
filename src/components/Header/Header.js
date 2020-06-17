c7ab65d7ebc939760ae971497c478584c910f510
unknown <buithebinh@gmail.com>
2020-06-17 16:47:49 +0700

    update header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,20 @@
 import React from "react";
 import "./Header.scss";
+import "../../style/styles.scss";
 import { NavLink } from "react-router-dom";
+import Logo from "../../assets/bubuedu.png"
 const Header = () => {
   return (
     <header className="header">
-      <div></div>
+      <div className="header__img" >
+        <img src={Logo} alt="Logo BubuEdu"/>
+      </div>
       <div>Categories</div>
       <div></div>
-      <NavLink to="/" className="btn btn--white header__btn header__btn--left">
+      <NavLink to="/" className="btn--white header__btn header__btn--left">
         Log in
       </NavLink>
-      <NavLink to="/" className="btn btn--darkgreen header__btn">
+      <NavLink to="/" className="btn--darkgreen header__btn" style={{color: 'white'}}>
         Sign up
       </NavLink>
     </header>
